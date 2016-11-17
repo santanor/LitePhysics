66f32ed53efb28d423e27608aa924d0bdf9978ee
Menti <josementi@gmail.com>
2016-11-17 01:00:35 +0000

    Three Newton laws implemented. Rocket engine component Basic gravitational system implementen within the physic components

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhysicsEngine : MonoBehaviour {
+
+    public float mass;              //[kg]
+    public Vector3 v;               //[m s^-1]
+    public Vector3 netForceVector;  //N []
+    private List<Vector3> forceVectorList = new List<Vector3>();
+    public bool showTrails = true;
+
+    private const float bigG = 6.673e-11f; 
+
+    private PhysicsEngine[] physicsEngineArray;
+    // Use this for initialization
+    void Start () {
+        SetupForceTrails();
+        physicsEngineArray = GameObject.FindObjectsOfType<PhysicsEngine>();
+	}
+	
+	void FixedUpdate()
+    {
+        RenderTrails();
+        CalculateGravity();
+        UpdatePosition();
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        forceVectorList.Add(force);
+    }
+
+    void CalculateGravity()
+    {
+        foreach(var pEngineA in physicsEngineArray)
+        {
+            foreach (var pEngineB in physicsEngineArray)
+            {
+                if(pEngineA != pEngineB)
+                {
+                    var offset = pEngineA.transform.position - pEngineB.transform.position;
+                    float rSquared = Mathf.Pow(offset.magnitude, 2f);
+                    float graviMagnitude = bigG * pEngineA.mass * pEngineB.mass / rSquared;
+                    Vector3 gravityFeltVector = graviMagnitude * offset.normalized;
+
+                    pEngineA.AddForce(-gravityFeltVector);
+                }
+            }
+        }
+    }
+
+    void UpdatePosition()
+    {
+        netForceVector = Vector3.zero;
+
+        foreach (Vector3 forceVector in forceVectorList)
+        {
+            netForceVector += forceVector;
+        }
+        forceVectorList = new List<Vector3>();
+        Vector3 acceleration = netForceVector / mass;
+        v += acceleration * Time.deltaTime;
+        transform.position += v * Time.deltaTime;
+    }
+       
+    
+    /// <summary>
+    /// Draw the trail
+    /// </summary>
+    private LineRenderer lineRenderer;
+    private int numberOfForces;
+
+    // Use this for initialization
+    void SetupForceTrails()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.SetColors(Color.yellow, Color.yellow);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        lineRenderer.useWorldSpace = false;
+    }
+
+    // Update is called once per frame
+    void RenderTrails()
+    {
+        if (showTrails)
+        {
+            lineRenderer.enabled = true;
+            numberOfForces = forceVectorList.Count;
+            lineRenderer.SetVertexCount(numberOfForces * 2);
+            int i = 0;
+            foreach (Vector3 forceVector in forceVectorList)
+            {
+                lineRenderer.SetPosition(i, Vector3.zero);
+                lineRenderer.SetPosition(i + 1, -forceVector);
+                i = i + 2;
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/RocketEngine.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class RocketEngine : MonoBehaviour {
+
+    public float fuelMass;              // [kg]
+    public float maxThrust;             // kN [kg m s^-2]
+    [Range(0,1f )]
+    public float thrustPercent;          // [none]
+    public Vector3 thrustUnitVector = Vector3.zero;  // 
+    private PhysicsEngine physicsEngine;
+
+    private float currentThrust;        //N
+
+    // Use this for initialization
+    void Start()
+    {
+        physicsEngine = GetComponent<PhysicsEngine>();
+        physicsEngine.mass += fuelMass;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(fuelMass > FuelThisUpdate())
+        {
+            fuelMass -= FuelThisUpdate();
+            physicsEngine.mass -= FuelThisUpdate();
+            ExertForce();
+        }else
+        {
+            Debug.LogWarning("Out of fuel");
+        }
+    }
+
+    float FuelThisUpdate()
+    {
+        float exhaustMassFlow;
+        float effectiveExhaustVelocity;
+
+        effectiveExhaustVelocity = 4462f;           // [m s^-1] 
+
+        //thrust = massFlow * exaustVelocity
+        exhaustMassFlow = currentThrust / effectiveExhaustVelocity;    
+
+        return exhaustMassFlow * Time.deltaTime;    //[kg]
+    }
+
+    void ExertForce()
+    {
+        currentThrust = thrustPercent * maxThrust * 1000f;//To get it in newtons
+        Vector3 thrustVector = thrustUnitVector.normalized * currentThrust;
+        physicsEngine.AddForce(thrustVector);
+    }
+}
